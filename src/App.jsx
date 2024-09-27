6ab912a6b86cf6e5567f4e347e7ea6587493084a
Sairam Sekhar Poluru <poluru.sairam@gmail.com>
2024-09-27 16:34:55 +0000

    Initialize React.js project with quick start template and basic setup.

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <h1>College Management System</h1>
+        <p>
+        The College Management System is a comprehensive web application designed to streamline various administrative tasks within a college environment. Built using React.js, this project provides an intuitive user interface for students, faculty, and administrators, allowing them to interact seamlessly with the system.
+        </p>
+      </header>
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
