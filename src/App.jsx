81083efff515ac5fa3264fbdb84c3f76c7e4cd7b
Sairam Sekhar Poluru <poluru.sairam@gmail.com>
2024-09-27 16:54:49 +0000

    updated the libraries

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
         <h1>College Management System</h1>
         <p>
-        The College Management System is a comprehensive web application designed to streamline various administrative tasks within a college environment. Built using React.js, this project provides an intuitive user interface for students, faculty, and administrators, allowing them to interact seamlessly with the system.
+        The College Management System is a comprehensive web application built using ReactJS.
         </p>
       </header>
       <main>
